fbd746b1a573e272bd27435de863cbab4a62a4a8
YENHUILEE <115877053+YENHUILEE@users.noreply.github.com>
2022-11-09 10:05:34 +0800

    Update NGS.sh

--- NGS.sh
+++ NGS.sh
@@ -75,7 +75,7 @@ module load biology/Picard/2.27.4
 picard FixMateInformation \
 INPUT=${temp_dir}/${sample_id}.${Date}.bwamem.marked.realigned.bam \
 OUTPUT=${temp_dir}/${sample_id}.${Date}.bwamem.marked.realigned.fixed.bam \
-SO=coordinate VALIDATION_STRINGENCY=LENIENT CREATE_INDEX=true && 
+SO=coordinate VALIDATION_STRINGENCY=LENIENT CREATE_INDEX=true 
 
 # #BaseRecalibrator (bwamem.marked.realigned.fixed.bam.recal_data.grp)
 # ml load biology/GATK/4.2.3.0
