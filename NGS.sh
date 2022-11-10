8f74245b5df9b681d198a3e4ee72aa98e8a044ce
YENHUILEE <115877053+YENHUILEE@users.noreply.github.com>
2022-11-11 00:52:37 +0800

    Update NGS.sh

--- NGS.sh
+++ NGS.sh
@@ -149,11 +149,12 @@ exec >$logfile 2>&1
 # --maxgenethread 20 --thread 20 
 
 #splice ai
-module load biology/Python/3.9.5
-module load biology/SpliceAI/1.3
+# module load biology/Python/3.9.5
 module load biology/Tensorflow/2.7.1
+module unload pkg/Anaconda3
+# module load biology/SpliceAI/1.3
 
-spliceai \
+python spliceai \
 -I ${release_dir}/${sample_id}.${Date}.annotate.hg19_multianno.vcf \
 -O ${release_dir}/${sample_id}.${Date}.annotate.hg19_multianno.spliceai.vcf \
 -R ${fasta} \
