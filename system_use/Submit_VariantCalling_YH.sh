b8ad3dbfc0dc7b600fa2117017503bec68fa7c34
YENHUILEE <k32650805@gmail.com>
2022-12-20 16:37:10 +0800

    Update Submit_VariantCalling_YH.sh

--- system_use/Submit_VariantCalling_YH.sh
+++ system_use/Submit_VariantCalling_YH.sh
@@ -7,6 +7,6 @@ DAY=`date +%Y%m%d`
 while read -r ID; 
 	do
 	rsync ${PIPELINE} ${workdir}/${DAY}_${ID}.sh 
-	sed -i "s|SAMPLE_NAME|${ID}|g" ${workdir}/${DAY}_${ID}.sh 
-	sleep 3s
+	sed -i "s|SAMPLE_ID|${ID}|g" ${workdir}/${DAY}_${ID}.sh 
+	sleep 1s
 	done<${SampleList}
