78aa80482c18b6f672b5c7bdb1b12ac08d30053d
YENHUILEE <k32650805@gmail.com>
2022-12-20 16:31:17 +0800

    Update Submit_VariantCalling_YH.sh

--- system_use/Submit_VariantCalling_YH.sh
+++ system_use/Submit_VariantCalling_YH.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/sh
+workdir =/home/u3003390/work/FASTQ
 SampleList=/home/u3003390/work/FASTQ/list.text
 PIPELINE=/home/u3003390/Script/nchc_script/workflow_me/NGS.sh
 DAY=`date +%Y%m%d`
 
 while read -r ID; 
 	do
-	rsync ${PIPELINE} ./${DAY}_${PIPELINE}_${ID}.sh 
-	sed -i "s|SAMPLE_NAME|${ID}|g" ./${DAY}_${PIPELINE}_${ID}.sh 
+	rsync ${PIPELINE} ${workdir}/${DAY}_${PIPELINE}_${ID}.sh 
+	sed -i "s|SAMPLE_NAME|${ID}|g" ${workdir}/${DAY}_${PIPELINE}_${ID}.sh 
 	sleep 3s
 	done<${SampleList}
