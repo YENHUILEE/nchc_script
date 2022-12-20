f6b2757d23b5f626196ebece048fe725445348ce
YENHUILEE <k32650805@gmail.com>
2022-12-20 17:20:14 +0800

    .

--- system_use/Submit_VariantCalling_YH.sh
+++ system_use/Submit_VariantCalling_YH.sh
@@ -8,5 +8,7 @@ while read -r ID;
 	do
 	rsync ${PIPELINE} ${workdir}/${DAY}_${ID}.sh 
 	sed -i "s|SAMPLE_ID|${ID}|g" ${workdir}/${DAY}_${ID}.sh 
+    echo "create sh"
 	sleep 1s
 	done<${SampleList}
+
